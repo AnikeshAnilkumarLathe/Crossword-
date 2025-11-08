e72b650a43d128523c8e2b0edb7bbc4d7e2ab94f
AnikeshAnilkumarLathe <anikeshlathe@gmail.com>
2025-11-09 05:05:52 +0530

    preloaderupdatefinal12

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   useEffect(() => {
     // Show loader on route change
     setLoading(true);
-    const timer = setTimeout(() => setLoading(false), 2000); 
+    const timer = setTimeout(() => setLoading(false), 2500); 
     return () => clearTimeout(timer);
   }, [location]);
 
