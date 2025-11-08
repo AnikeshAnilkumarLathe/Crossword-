f7d14ec7c0616d1752a5f9b5e3dd835d2e41dd59
AnikeshAnilkumarLathe <anikeshlathe@gmail.com>
2025-11-08 06:47:38 +0530

    cellnumbering4

--- src/components/CrosswordPage.jsx
+++ src/components/CrosswordPage.jsx
@@ -428,7 +428,7 @@ export default function CrosswordPage() {
                     const isBlank = cellData?.IsBlank;
                     const number = cellData?.NumberAssociated;
                     return (
-                      <div key={c} className={`cell ${isBlank == null ? "white" : "black"}`}>
+                      <div key={c} className={`cell ${isBlank === true ? "white" : "black"}`}>
                         {cellData !== null && number>0  && (
                           <span className="cell-number"> {number} </span>
                         )}
